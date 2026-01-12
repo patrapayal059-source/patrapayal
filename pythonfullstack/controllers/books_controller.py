9e4953776ff93a2c0c8bd920aa43fc26c35b5f39
patrapayal059-source <patrapayal059@gmail.com>
2026-01-12 04:34:26 +0000

    FINAL PROJECT

--- pythonfullstack/controllers/books_controller.py
+++ pythonfullstack/controllers/books_controller.py
@@ -2,24 +2,18 @@ from flask import Blueprint, render_template, request, redirect, url_for
 
 books_bp = Blueprint("books", __name__, url_prefix="/books")
 
-BOOKS = [
-    "Python Basics",
-    "Flask Web Development",
-    "Django for Beginners",
-    "Data Structures",
-    "Machine Learning"
-]
+BOOKS = ["Python Basics", "Flask Web Development", "Django for Beginners","c++","java"]
 
-@books_bp.route("/", methods=["GET"])
+@books_bp.route("/", methods=["GET", "POST"])
 def books():
+    if request.method == "POST":
+        book_name = request.form.get("book_name")
+        if book_name:
+            BOOKS.append(book_name)
+        return redirect(url_for("books.books"))
+
     return render_template("books.html", books=BOOKS)
 
-@books_bp.route("/add", methods=["POST"])
-def add_book():
-    book_name = request.form.get("book_name")
-    if book_name:
-        BOOKS.append(book_name)
-    return redirect(url_for("books.books"))
 
 @books_bp.route("/delete/<int:book_id>")
 def delete_book(book_id):
